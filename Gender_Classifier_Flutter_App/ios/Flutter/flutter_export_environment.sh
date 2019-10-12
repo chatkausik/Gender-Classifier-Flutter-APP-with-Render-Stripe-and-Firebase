19d4add93729a359d2c3c5f4d07093172cc1bf39
Kausik Chattapadhyay <chat.kausik@gmail.com>
2019-10-12 18:19:32 -0400

    Add files via upload

---
+++ Gender_Classifier_Flutter_App/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kausik/Desktop/development/flutter-2"
+export "FLUTTER_APPLICATION_PATH=/Users/kausik/Desktop/Make_Money_with_Siraj/Midterm/Gender Classification/gender classifier App"
+export "FLUTTER_TARGET=/Users/kausik/Desktop/Make_Money_with_Siraj/Midterm/Gender Classification/gender classifier App/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kausik/Desktop/development/flutter-2/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
